35c1225ac50742f564c7d2fdff67d2bb7a02f2e0
Lokisfeuer <thede.v.oertzen@web.de>
2024-01-04 17:12:13 +0100

    completely updated adventure.

--- main.py
+++ main.py
@@ -1,64 +1,94 @@
-import pickle
+import json
+from encode_adventure import Adventure, all_features
 
 
-# TODO: Make a better API!
-# this is the API-function for the webapp.
 def get_adventure(username, adventure, object_type, prefill, to_save):
-    # TODO: use proper adventure structure instead of pickle.
-    all_objects = setup()
-    all_objects = {key[0:3].lower(): value for key, value in all_objects.items()}
-    if isinstance(object_type, str):
-        object_type = object_type.lower()
-    with open('test.pickle', 'rb') as handle:
-        full_data = pickle.load(handle)
-    if username not in full_data.keys():
-        user_data = {i[0:3].lower(): [] for i in all_objects.keys()}
-        full_data.update({username: user_data})
-    if to_save is not None:
-        full_data[username][object_type].append(to_save)
-        with open('test.pickle', 'wb') as handle:
-            pickle.dump(full_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        object_type = None
+    with open('user_to_path.json', 'r') as f:
+        paths = json.load(f)
+    if username not in paths.keys():
+        adv = Adventure(f'First Adventure {username}')
+        adv.save(f'user_written_adventures/{adv.name}')
+        paths.update({username: {adv.name: f'user_written_adventures/{adv.name}'}})
+    if adventure is not None:
+        if adventure not in paths[username]:
+            adv = Adventure(f'{adventure} {username}')
+            adv.save(f'user_written_adventures/{adv.name}')
+            paths[username].update({adventure: f'user_written_adventures/{adv.name}'})
+    else:
+        adventure = f'First Adventure {username}'
+    adv = Adventure(adventure)
+    adv.load(f'{paths[username][adventure]}')  # adventure=None works fine here.
+    current_object = ''
     if object_type is not None:
-        if prefill is None:
-            features = all_objects[object_type].features
-            current_object = {i: '' for i in features.keys()}
-        else:
-            if isinstance(prefill, bool):
-                if prefill:
-                    features = all_objects[object_type].features
-                    current_object = {i: 'something' for i in features.keys()}
+        name_to_feat = {'spi': adv.sci, 'mot': adv.mot, 'eve': adv.eus, 'npc': adv.npc, 'geh': adv.geh, 'gru': adv.gru,
+                        'bea': adv.bea, 'geg': adv.geg}
+        cla = name_to_feat[object_type]
+        if to_save is not None:
+            objs = all_features[cla.name]
+            vals = {}
+            for feat, val in to_save.items():
+                if feat in objs['texts']:
+                    vals.update({feat: val})
+                elif feat in objs['single_ids']:
+                    # check for ids
+                    vals.update({feat: val})
+                elif feat in objs['bools']:
+                    if val.lower().startswith(('y', 'yes', 'yup', 'yeah', '1')):
+                        vals.update({feat: True})
+                    else:
+                        vals.update({feat: False})
+                elif feat in objs['scalars']:
+                    try:
+                        vals.update({feat: float(val)})
+                    except ValueError:
+                        vals.update({feat: 0.0})
+                elif feat in objs['list_ids']:
+                    vals.update({feat: val.split(', ')})
                 else:
-                    features = all_objects[object_type].features
-                    current_object = {i: '' for i in features.keys()}
+                    raise ValueError
+            cla.add(**vals)
+            # maybe change datatypes here and there.
+        else:
+            if isinstance(prefill, str):
+                pass  # TODO edit object
+            elif prefill is None:
+                pass  # This case only comes to play with the / route.
+            elif prefill:
+                current_object = {}
+                for i in cla.features.keys():
+                    objs = all_features[cla.name]
+                    if i in objs['texts']:
+                        current_object.update({i: '"This is awesome and definitely AI-generated text!"'})
+                    elif i in objs['single_ids']:
+                        current_object.update({i: 'id_spi_1'})
+                    elif i in objs['bools']:
+                        current_object.update({i: 'True'})
+                    elif i in objs['scalars']:
+                        current_object.update({i: '1.0'})
+                    elif i in objs['list_ids']:
+                        current_object.update({i: 'id_spi_1, id_spi_2, id_spi_3'})
+                    else:
+                        raise ValueError
             else:
-                # this will only work when working with the real adventure because without the objects don't exist have ids'
-                for i in full_data[username][object_type]:
-                    break
-                    if i['id'] == prefill:
-                        full_data[username][object_type].remove(i)
-                        current_object = i
-    else:
-        current_object = 'To create an object or edit one click the respective button.'
-    written_adventure = f'This is the long and detailed written adventure {adventure} from user {username}.'
-    listed_adventure = f'This is the long and detailed listed listed adventure {adventure} from user {username}.\n\n{full_data}'
-    return current_object, listed_adventure, written_adventure
-
-    # to_save contains an object that is to be saved to the adventure.
-    if object_type is None:
-        current_object = 'To create an object or edit one click the respective button.'
-    else:
-        # object_type is first three letters of id.
-        # create current object using everything including prefill.
-        current_object = None
-        # if prefill is None -> make dictionary with blank values.
-        # if prefill is True -> generate prefill values from AI.
-        # if prefill is string -> load that object, delete it from adventure and use its features as prefill values.
-        # either way current_object is a dict.
-    # load adventure by username and ggf. adventure
-    written_adventure = f'This is the long and detailed written adventure {adventure} from user {username}.'
-    listed_adventure = f'This is the long and detailed listed listed adventure {adventure} from user {username}.'
-    return current_object, listed_adventure, written_adventure
+                current_object = {}
+                for i in cla.features.keys():
+                    objs = all_features[cla.name]
+                    if i in objs['texts']:
+                        current_object.update({i: '"some text here"'})
+                    elif i in objs['single_ids']:
+                        current_object.update({i: 'id_obj_nr'})
+                    elif i in objs['bools']:
+                        current_object.update({i: 'False'})
+                    elif i in objs['scalars']:
+                        current_object.update({i: '0.0'})
+                    elif i in objs['list_ids']:
+                        current_object.update({i: '"id_obj_nr, id_obj_nr, ..."'})
+                    else:
+                        raise ValueError
+    adv.save(f'{paths[username][adventure]}')
+    with open('user_to_path.json', 'w') as f:
+        f.write(json.dumps(paths, indent=4))
+    return current_object, adv.to_list(), adv.to_text()
 
 
 if __name__ == '__main__':
