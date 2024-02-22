5fd0db7a144cf85b118b59d38f5f06de73f1ac75
Lokisfeuer <thede.v.oertzen@web.de>
2024-02-22 13:58:59 +0100

    Complete and clean pseudocode. Needs testing.

--- adventure.py
+++ adventure.py
@@ -2,26 +2,37 @@
 # Therefore, this file defines the general structure of an adventure
 
 import this
+from collections import namedtuple
+import inspect
 import json
 
 
 class Adventure:
-    def __init__(self): # name?
-        self.all_objects = []
-        pass
+    def __init__(self, filename=None):  # name of adventure? I think names are more confusing than they help.
+        if filename is None:
+            self.all_units = {}
+        else:
+            self.all_units = self.load(filename)
 
-    def __getitem__(self, key):
-        return self.all_objects[key]
+    def __getitem__(self, unit_id):
+        # this was changed recently.
+        return self.all_units[unit_id['unit_type']][unit_id['nr']]
+
+    def __len__(self):
+        n = 0
+        for i in self.all_units.values():
+            for _ in i:
+                n += 1
+        return n
 
     # add magic methods?
 
-    def add_object(self, object_type, feature_values):
-        # only use this function to create a new object or add one to the adventure.
-        object_id = len(self.all_objects)
-        object = None  # generate object of object type
-        self.all_objects.append(object)
-        return object  # is this a good idea?
-
+    def add_unit(self, unit):
+        if type(unit).__name__ not in self.all_units.keys():
+            self.all_units.update({type(unit).__name__: []})
+        self.all_units[type(unit).__name__].append(unit)
+        unit_id = (type(unit).__name__, len(self.all_units[type(unit).__name__]))  # this line is pretty.
+        return unit_id
 
     def save(self, filename):
         adventure = self.to_dict()
@@ -29,41 +40,104 @@ class Adventure:
             json.dump(adventure, outfile)
 
     def load(self, filename):
+        # load the adventure in the file
+
+        # read file
         with open(filename) as adventure_file:
             adventure = json.load(adventure_file)
-        # set self attributes to what was loaded
+
+        all_units = {}  # this function gets called before self.all_units gets initialized
+        for unit_type in adventure:
+            all_units.update({unit_type: []})
+            for feature_values in adventure[unit_type]:
+                unit_class = globals()[unit_type]  # get specific unit class
+                unit = unit_class(feature_values)  # create unit object
+                all_units[unit_type].append(unit)  # write all_units to how it should be
+        # self.all_units = all_units  # I think this line is not nice, but I am unsure. Redundant with __init__
+        return all_units
 
     def to_dict(self):
         # return the adventure as a json object without saving it.
-        pass
+        adventure = {}
+        for unit_type, list_of_units in self.all_units.items():
+            adventure.update({unit_type: []})
+            for unit in list_of_units:
+                adventure[unit_type].append(unit.to_dict())
+        return adventure
 
     def to_vector(self):
         # return the vector representation of the Adventure.
-        for i in self.all_objects:
-            i.pre_encode()
-        for i in self.all_objects:
-            i.real_encode(self)
+
+        # generate pre encodings
+        for list_of_units in self.all_units.values():
+            for unit in list_of_units:
+                unit.pre_encode()
+
+        # get real encodings
+        real_encodings = {}
+        for unit_type, list_of_units in self.all_units.items():
+            real_encodings.update({unit_type: []})
+            for unit in unit_type:
+                # which version is cleaner?
+                # real_encodings[unit_type].append(unit.real_encode())
+                unit.real_encode()
+                real_encodings[unit_type].append(unit.real_encoding)
+
+        # todo:
         # feed real encodings into AI
-        pass
+        # concat results
+        # feed results into AutoEncoder AI
+        # return vector representation
 
     def to_text(self):
-        raise NotImplementedError
+        raise NotImplementedError('Adventure to text doesn\'t really work yet.')
 
 
 class Unit:
-    def __init__(self, feature_values):
-        # only call this function from the adventure
-        # check feature_values datatypes
+    def __init__(self, **feature_values):
+        self.features = None
+        self.set_features()
+        self.check_datatypes(feature_values)
+        self.feature_values = feature_values
         self.pre_encoding = None
         self.real_encoding = None
-        pass
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        if self.feature_values != other.feature_values:
+            return False
+        # This function ignores encodings. I am not sure whether that is good or bad.
+        return True
 
     def to_dict(self):  # ? add "_" in front ?
         # return a dictionary version of the object.
-        pass
+        return self.feature_values  # I am unsure if this is perfect.
 
-    # maybe add a load function
+    # no load function needed
 
+    def check_datatypes(self, feature_values):
+        # features is a dict structured like this: {"feature_name": data_type_feature_should_be}.
+        # if feature should be a list instead of the data_type features contains a tuple (list, element_data_type).
+        for feature, val in feature_values.items():
+            if feature not in list(self.features.keys()):
+                raise ValueError(f'Feature "{feature}" does not exist.')
+
+            elif isinstance(self.features[feature], tuple):
+                if not isinstance(val, list):
+                    raise ValueError(f'Feature "{feature}" should be of type "list".')
+
+                if len(val) == 0:
+                    pass
+                elif not isinstance(val[0], self.features[feature][1]):
+                    raise ValueError(f'Elements of feature "{feature}" (which is a list) '
+                                     f'should be of type "{self.features[feature][1]}"')
+
+            elif not isinstance(val, self.features[feature]):
+                raise ValueError(f'Feature "{feature}" should be of type "{self.features[feature]}".')
+        return
+
+    # overwrite the following functions
     def pre_encode(self):
         # generate the pre-encoding for this object
         self.pre_encoding = None
@@ -73,8 +147,48 @@ class Unit:
         # get pre encodings through the adventure.
         # use AI (either Transformer or RNN) to interpret list of pres.
         self.real_encoding = None
-        pass
+        return self.real_encoding
+
+    def set_features(self):
+        return {}
+
+
+# advantage of this is mostly, that now type(id) does not produce integer or string but UnitId.
+UnitId = namedtuple('UnitId', 'unit_type nr')
+
+
+class NotPlayerCharacter(Unit):
+    def check_datatypes(self, feature_values):
+        if type(feature_values['name']) is not str:
+            raise ValueError(f"Name must be a string. Got {type(feature_values['name'])} instead.")
+        return
+
+    def set_features(self):
+        self.features = []
+
+all_unit_types = [NotPlayerCharacter]
 
 
 def write_demo_adventure():
     pass
+
+
+
+def quicktest():
+    all_globals = globals()
+    global_classes = [x for x in all_globals.values() if inspect.isclass(x)]
+    global_classes = global_classes[3:]
+    # global_classes is now a list of all Unit Types (class objects for each)
+    print(global_classes)
+
+    id1 = UnitId('type', 3)
+    print(type(id1))
+    npc = NotPlayerCharacter(first=1, second=2, name='John')
+    print(npc == 'thisstring')
+    print(npc.real_encoding)
+    x = type(npc).__name__
+    print([type(npc)])
+
+
+if __name__ == "__main__":
+    quicktest()
--- write_advs.py
+++ write_advs.py
@@ -1,93 +1,142 @@
-# import old_encode_adventure
 import random
-import adventure
+
+from adventure import *
+import pandas as pd
+
+
+# import adventure
+# from adventure import all_unit_types
+
+
+def multi_for_loop(iterations):
+    # a generator to iterate like multiple for loops but the amount is dynamic
+    indeces = [0 for _ in iterations]
+    if 0 in iterations:
+        raise ValueError(f'Amount of iterations must for all instances be greater than zero. '
+                         f'Received the following amounts of iterations per instance: {iterations}')
+    while True:
+        indeces[-1] += 1
+        x = 0
+        for i in range(len(indeces)):
+            idx = indeces[-(i + 1)]
+            max = iterations[-(i + 1)]
+            x += 1
+            if idx == max:
+                indeces[-x] = 0
+                if x == len(indeces):
+                    return
+                indeces[-(x + 1)] += 1
+        yield indeces
+
+
+'''
+possible feature types:
+    - float (/ Integer)
+    - boolean
+    - string
+    - UnitId
+    - list
+'''
 
 
 def adventure_pre_ai():
-    # write the giant adventure like in old_encode_adventure the generate_adventure_objs() function
-    adventure = None
-    return adventure
+    # write a giant adventure to train the AI that will interpret lists of pre-encodings.
+    # like in old_encode_adventure the generate_adventure_objs() function
+
+    # load and prepare twitter dataset to use for texts
+    twitter = pd.read_csv('twitter_data.csv')
+    twitter = twitter['clean_text']
+    twitter = twitter.dropna(axis=0, how='all')
+    twitter = twitter.reset_index(drop=True)
+    twitterator = 5
+
+    # all possible entries for each feature datatype.
+    options = \
+        {
+            bool: [True, False],  # there are 2 options for boolean features
+            float: [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            # there are infinite options for floats. I round them to 10 options
+            int: list(range(10))  # there are infinite options for integers. I just take the first 10 options.
+            # there are infinite options for strings. I take only one but that is always a unique tweet from Twitter.
+        }
+    mega_adventure = Adventure()
+    for unit_type in all_unit_types:
+
+        # get options for each feature of this unit type.
+        features = unit_type().features
+        specific_options = []
+        for feature, feature_type in features.items():
+            if feature_type in options.keys():
+                specific_options.append(options[feature_type])
+
+        # iterate over every possible combination of the options (like potenzmenge = power set)
+        iterations = [len(i) for i in specific_options]
+        for indeces in multi_for_loop(iterations):
+            n = 0
+            choice = {}  # the option taken for the unit about to be created.
+            for feature, feature_type in features.items():
+                if feature_type in options.keys():
+                    # nth feature in the options
+                    choice.update({feature: specific_options[indeces[n]]})
+                    n += 1
+                elif feature_type is str:
+                    # take a tweet from Twitter as text.
+                    choice.update({feature: twitter[twitterator]})
+                    twitterator += 1
+                elif feature_type is list:
+                    pass  # can probably ? be ignored
+                    # because elements of this adventure are intentionally created for pre-encodings
+                    # and lists of UnitIds are usually irrelevant then.
+                elif feature_type is UnitId:
+                    pass  # can be ignored for pre-encodings
+                else:
+                    raise ValueError('Something went unexpectedly wrong. ')
+            mega_adventure.add(unit_type(**choice))
+    return mega_adventure
 
 
-def many_random(giant_adventure):  # TODO: find better name for giant_adventure
-    advs = []
-    for i in range(10000):
-        adv = encode_adventure.Adventure(f'DataAdventure{i}')
-        name_to_feat = {'sci': adv.sci, 'mot': adv.mot, 'eus': adv.eus, 'npc': adv.npc, 'geh': adv.geh,
-                        'gru': adv.gru, 'bea': adv.bea, 'geg': adv.geg}
-        this = random.sample(all_ids, random.randint(15, 30))
-        for i in this:
-            f_v = add_references(giant_adv, i, [j for j in this if j != i])
-            _, feat, nr = tuple(i.split('_'))
-            name_to_feat[feat].add(**f_v)
-        advs.append(adv)
-        pass
+def all_units(adventure):
+    for i in adventure.all_units.values():
+        for j in i:
+            yield j
 
 
-
-all_features = {
-    'sci': {
-        'texts': ['name', 'backstory'],
-        'bools': ['charakterbogen', 'plaene_fuer_den_charakter', 'hat_eine_backstory'],
-        'scalars': [],
-        'single_ids': [],
-        'list_ids': ['startszene', 'events', 'gruppen', 'backstory_sonstiges']
-    },
-    'eus': {
-        'texts': ['was', 'warum'],
-        'bools': ['untersuchen', 'soziale_interaktion', 'fight', 'start'],
-        'scalars': ['schwierigkeitsgrad', 'wahrscheinlichkeit'],
-        'single_ids': [],
-        'list_ids': ['wer', 'wo', 'Gegenstände', 'Geheimnisse', 'personen', 'wer_muss_da_sein', 'wo_kann_das_sein',
-                     'motivationen']
-    },
-    'npc': {
-        'texts': ['name', 'backstory'],
-        'bools': ['charakterbogen', 'plaene', 'hat_eine_backstory'],
-        'scalars': [],
-        'single_ids': [],
-        'list_ids': ['events_und_szenen', 'gruppen', 'backstory_sonstiges']
-    },
-    'geh': {
-        'texts': ['was'],
-        'bools': [],
-        'scalars': ['positivitaet'],
-        'single_ids': [],
-        'list_ids': ['wer_weiss_davon', 'wen_und_was_betrifft_das']
-    },
-    'gru': {
-        'texts': ['grund_des_zusammenhalts'],
-        'bools': [],
-        'scalars': [],
-        'single_ids': ['moegliche_motivation_von_aussen', 'geburtsort_der_gruppe'],
-        'list_ids': []
-    },
-    'geg': {
-        'texts': ['was'],
-        'bools': [],
-        'scalars': ['wert'],
-        'single_ids': [],
-        'list_ids': ['wessen', 'wo']
-    }
-}
+def all_pre_encodings():
+    all_pres = []
+    adventure = adventure_pre_ai()
+    for i in all_units(adventure):
+        i.pre_encode()
+        all_pres.append(i.pre_encoding)
+    return all_pres
 
 
-def get_f_v_by_id(id, adv):
-    name_to_feat = {'sci': adv.sci, 'mot': adv.mot, 'eus': adv.eus, 'npc': adv.npc, 'geh': adv.geh, 'gru': adv.gru,
-                    'bea': adv.bea, 'geg': adv.geg}
-    _, feat, nr = tuple(id.split('_'))
-    feat = name_to_feat[feat]
-    f_v = feat.all_objects[nr]
-    del f_v['ID']
-    return f_v
+def many_smaller_adventure(n=10000):
+    # generate n adventures of a realistic size. Every object has some relation to the others.
+    # purpose is to train the real_encoding function with these adventures as data.
+    all_adventures = []
+    units = list(all_units)  # the list of all units from adventure_pre_ai
+    for i in range(n):
+        units_for_this_adventure = random.sample(units, random.randint(15, 30))
 
+        # generate a list of all ids that could appear in this adventure.
+        id_list = []
+        adventure = Adventure()
+        for unit in units_for_this_adventure:
+            id_list.append(adventure.add_unit(unit))  # adventure.add_unit gives back the UnitId
 
-def add_references(giant_adv, main_id, other_ids):
-    _, feat, nr = tuple(main_id.split('_'))
-    f_v = get_f_v_by_id(main_id, giant_adv)
-    feats = all_features[feat]
-    for i in feats['single_ids']:
-        f_v[i] = random.choice(other_ids)
-    for i in feats['list_ids']:
-        f_v[i] = random.sample(other_ids, random.randint(3, 10))
-    return f_v
+        adventure = Adventure()
+        for unit in units_for_this_adventure:  # iterate over every unit
+            feature_values = unit.feature_values  # get feature values
+            for feature, feature_type in unit.features.items():
+                #  add relations between units to feature values
+                if feature not in feature_values.keys():
+                    if feature_type == UnitId:
+                        feature_values.update({feature: random.choice(id_list)})
+                    elif feature_type == tuple:  # the actual feature should in this case be a list
+                        feature_values.update({feature: random.sample(id_list, random.randint(3, 10))})
+                    else:
+                        raise ValueError('Something went wrong unexpectedly. Please fix.')
+
+            adventure.add_unit(unit.__class__(**feature_values))  # recreate unit with relations.
+        all_adventures.append(adventure)
+    return all_adventures
