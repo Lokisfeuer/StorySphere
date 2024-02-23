34dd01393c0ae3e80d34540afec988cee96f95c3
Lokisfeuer <thede.v.oertzen@web.de>
2024-02-23 18:53:12 +0100

    minor changes

--- train_ais.py
+++ train_ais.py
@@ -1,12 +1,12 @@
 from write_advs import *
-from rnn import train_model, acc
+import rnn as rnn
 import autoencoder as autoencoder
 import torch
 
 
 def anna():
     data = all_pre_encodings()
-    encoder, decoder = train_model(data)
+    encoder, decoder = rnn.train_model(data)
     torch.save({encoder, f'anna_encoder.pt'})
     torch.save({decoder, f'anna_decoder.pt'})
     return encoder, decoder
@@ -17,7 +17,7 @@ def bernd():
     # split by types.
     bernds = {}
     for unit_type, data_set in real_encodings.items():
-        encoder, decoder = train_model(data_set)
+        encoder, decoder = rnn.train_model(data_set)
         bernds.update({unit_type: data_set})
         torch.save(encoder, f'bernd_encoder_{unit_type}.pt')  # if unit_type is not a string, then take .__name__
         torch.save(decoder, f'bernd_decoder_{unit_type}.pt')
@@ -28,15 +28,16 @@ def charlene():
     data = []
     for i in many_small_adventures():
         data.append(i.to_vector(use_autoencoder=False))
-    encoder, decoder = autoencoder.train(data)
+    encoder, decoder = autoencoder.train_model(data)
     torch.save(encoder, f'charlene_encoder.pt')
     torch.save(decoder, f'charlene_decoder.pt')
     # train autoencoder
 
 
 if __name__ == '__main__':
-    # get twitter dataset ready
+    # TODO:
     # add different unit types and their features
+    # test
     anna()
     bernd()
     charlene()
