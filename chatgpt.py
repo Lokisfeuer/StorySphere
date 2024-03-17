6c2da4cdd69276d6c7d196f08e34e1aeb2dd3a8f
Lokisfeuer <thede.v.oertzen@web.de>
2024-03-17 11:03:52 +0100

    replaced chatgpt.py file with openai_call.py

--- chatgpt.py
+++
@@ -1,9 +0,0 @@
-from adventure import UnitId
-
-
-def ask_chatgpt(old_adventure, newunit, id_to_name):
-    adventure_text = old_adventure.to_text(id_to_name)
-    unit_text = newunit.to_text(id_to_name)
-    text = f'This is a story categorized in different units of different types:\n' \
-           f'{adventure_text}\n' \
-           f'I want this story to be a balanced '
